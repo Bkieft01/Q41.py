b4d47ca33dcc55fa70643873736ca2d40a8e02c0
Bkieft01 <116841775+Bkieft01@users.noreply.github.com>
2022-10-28 12:54:58 -0400

    Add files via upload

---
+++ Q41.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (v3.10.7:6cc6b13308, Sep  5 2022, 14:02:52) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Brenna Q41
+>>> def count_hashtag(string,target):
+...     total+0
+...     index+0
+...     while index<len(string):
+...         if string[index:index+len(target)]==target:
+...             total+=1
+...             index+= len(target)
+...         else:
+...             index+=1
+...     return total
+... 
